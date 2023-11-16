af89fe797ae4d150f5442c989ea6a0f75b6b7d92
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2023-11-16 14:56:11 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 const randomFolks = document.querySelector(".random-peeps");
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    //console.log(data);
+    const userResults = data.results;
+};
+
+getData();
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        
+    };
+};
+
